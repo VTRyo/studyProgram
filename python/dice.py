96028e46bbf25327ac3ab1e72c4460eafa44283a
inaba <vtryo.work@gmail.com>
2017-10-13 00:41:09 +0000

    add dice.py for val

--- python/dice.py
+++ python/dice.py
@@ -5,3 +5,21 @@
 #     def shoot(self):
 #         return random.randint(1,self.face_num)
 
+# import random
+
+# class Dice:
+#     face_num = 6
+#     def __init__(self):
+#         print('Hello')
+        
+#     def shoot(self):
+#         return random.randint(1,self.face_num)
+
+import random
+
+class Dice():
+    def __init__(self,val):
+        self.face_num = val
+        
+    def shoot(self):
+        return random.randint(1,self.face_num)
