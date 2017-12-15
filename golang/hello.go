62a72640299d4f1854c4841fb3c977e73a271b18
RYO <vtryo.work@gmail.com>
2017-12-15 15:11:47 +0900

    study golang

---
+++ golang/hello.go
@@ -0,0 +1,44 @@
+/*
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("hello world")
+}
+*/
+package main
+
+import "fmt"
+
+func main() {
+  // var msg string
+  // msg = "hello"
+  msg := "hello"
+  fmt.Println(msg)
+}
+
+//基本的なデータ型
+/*
+string "hello"
+int 53
+float64 10.2
+bool false true
+nil
+
+var a string // ""
+var a int // 0
+var f bool // false
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+  a := 10
+  b := 12.3
+  c := "hoge"
+  var d bool
+  fmt.Println("a: %d, b:%f, c:%s, d:%")
+}
