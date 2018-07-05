119fa8964802f741e32a7f3c1fb5e817b3f4c8cd
inaba <vtryo.work@gmail.com>
2018-07-05 09:45:48 +0000

    tutorial chapter 2 done

--- rails/toy_app/db/schema.rb
+++ rails/toy_app/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180214140049) do
+ActiveRecord::Schema.define(version: 20180705075638) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
