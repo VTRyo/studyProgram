4c93533b2710ed35f574ff5ae3a10560ee3464d7
inaba <vtryo.work@gmail.com>
2018-08-03 07:58:05 +0000

    chapter 6.3.3 wip

--- rails/toy_app/test/models/user_test.rb
+++ rails/toy_app/test/models/user_test.rb
@@ -42,4 +42,18 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
+  
+  def setup
+    @user = User.new(name: "Examlple User", email: "user@example.com",
+              password: "foobar", password_confirmation: "foobar")
+  end
+  
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+  
+  test "password should be have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+  end
 end
