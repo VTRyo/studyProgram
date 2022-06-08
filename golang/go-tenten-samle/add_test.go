18a9fd5345dd17b20c3e63586af4d008230a54de
VTRyo <vtryo.work@gmail.com>
2022-06-08 22:39:58 +0900

    fatal case

--- golang/go-tenten-samle/add_test.go
+++ golang/go-tenten-samle/add_test.go
@@ -18,6 +18,11 @@ func TestAdd(t *testing.T) {
 		want int
 	}{
 		{
+			name: "fatal",
+			args: args{a: 1, b: 2},
+			want: 30,
+		},
+		{
 			name: "normal",
 			args: args{a: 1, b: 2},
 			want: 3,
@@ -25,9 +30,17 @@ func TestAdd(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			t.Cleanup(func() {
+				t.Log("cleanup")
+			})
+
+			defer t.Log("defer")
+
 			if got := add(tt.args.a, tt.args.b); got != tt.want {
-				t.Errorf("add() = %v, got %v", got, tt.want)
+				t.Fatalf("add() = %v, got %v", got, tt.want)
 			}
+
+			t.Log("no call")
 		})
 	}
 }
