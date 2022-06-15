abc673d448e66f5190fd00a50bd1dca02ed06bb2
VTRyo <vtryo.work@gmail.com>
2022-06-15 22:19:26 +0900

    chはファーストクラスオブジェクト

--- golang/go-tenten-samle/goroutine/ch.go
+++ golang/go-tenten-samle/goroutine/ch.go
@@ -32,3 +32,32 @@ func ChanSeletct() {
 		fmt.Println(v2)
 	}
 }
+
+func ChanNil() {
+	ch1 := make(chan int)
+	var ch2 chan string //ゼロ値はnil
+
+	go func() { ch1 <- 100 }()
+	go func() { ch2 <- "hi" }()
+
+	select {
+	case v1 := <-ch1:
+		fmt.Println(v1)
+	case v2 := <-ch2:
+		fmt.Println(v2)
+	}
+}
+
+func makeCh() chan int {
+	return make(chan int)
+}
+
+func recvCh(recv chan int) int {
+	return <-recv
+}
+
+func ArgChan() {
+	ch := makeCh()
+	go func() { ch <- 100 }()
+	fmt.Println(recvCh(ch))
+}
